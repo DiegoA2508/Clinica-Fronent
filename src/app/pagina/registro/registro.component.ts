3c35a0b463254925723d077dea496216acb16e7e
DiegoA2508 <diegoa.zunigam@uqvirtual.edu.co>
2023-11-02 20:05:50 -0500

    Se agregan las plantillas y estilos de Boostrap

--- src/app/pagina/registro/registro.component.ts
+++ src/app/pagina/registro/registro.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { RegistroPacienteDTO } from 'src/app/modelo/registro-paciente-dto';
 
 @Component({
   selector: 'app-registro',
@@ -6,5 +7,37 @@ import { Component } from '@angular/core';
   styleUrls: ['./registro.component.css']
 })
 export class RegistroComponent {
+
+  registroPacienteDTO: RegistroPacienteDTO;
+  ciudades:string[];
+
+  constructor(){
+    this.registroPacienteDTO= new RegistroPacienteDTO();
+    this.ciudades = [];
+    this.cargarCiudades();
+  }
+
+  public registrar(){
+    console.log(this.registroPacienteDTO);
+  }
+
+  public sonIguales():boolean{
+    return this.registroPacienteDTO.password == this.registroPacienteDTO.confirmaPassword;
+  }
+
+  private cargarCiudades(){
+    this.ciudades.push("Armenia");
+    this.ciudades.push("Calarcá");
+    this.ciudades.push("Pereira");
+    this.ciudades.push("Manizales")
+    this.ciudades.push("Medellin")
+  }
+
+  public onFileChange(event:any){
+    if(event.target.files.length > 0){
+    const files = event.target.files;
+    console.log(files);
+    }
+  }
 
 }
